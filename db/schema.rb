5f62698c40033e46a83f6081ef57ff804d44890f
Stephanie Vander Elst <stephvanderelst@gmail.com>
2018-11-05 15:27:39 +0100

    Buyer, Provider, Profile models created with links to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_05_135103) do
-
+ActiveRecord::Schema.define(version: 2018_11_05_141602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.text "description"
+    t.string "status"
+    t.integer "price"
+    t.integer "hours"
+    t.date "date"
+    t.string "city"
+    t.string "street"
+    t.string "number"
+    t.integer "zip_code"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "buyers", force: :cascade do |t|
+    t.boolean "status"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_buyers_on_user_id"
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.boolean "status"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_providers_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,4 +64,6 @@ ActiveRecord::Schema.define(version: 2018_11_05_135103) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "buyers", "users"
+  add_foreign_key "providers", "users"
 end
